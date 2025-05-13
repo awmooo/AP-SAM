01e0d86a71bed01b15c588025b8436fca1b7c1b7
awmooo <91874044+awmooo@users.noreply.github.com>
2025-05-13 21:07:46 +0800

    Delete AP-SAM directory

--- AP-SAM/eval_ac_sam.sh
+++
@@ -1,6 +0,0 @@
-torchrun  --nproc_per_node=2 train_AC-SAM.py \
---checkpoint ../pretrained_checkpoint/sam_vit_b_01ec64.pth \
---model-type vit_b \
---output /home/upc/Documents/SAM/sam-hq-main/sam-hq-main/train/AC-SAM/work_dirs/ap_sam_b_cnn_savebest_warm_768_lessfeat/vis \
---eval --restore-model /home/upc/Documents/SAM/sam-hq-main/sam-hq-main/train/AC-SAM/work_dirs/ap_sam_b_cnn_savebest_warm_768_lessfeat/best_model.pth \
-#--visualize
--- AP-SAM/outmask_ac_sam.sh
+++
@@ -1,6 +0,0 @@
-torchrun  --nproc_per_node=2 output_binary_masks.py \
---checkpoint ../pretrained_checkpoint/sam_vit_b_01ec64.pth \
---model-type vit_b \
---output /home/upc/Documents/SAM/sam-hq-main/sam-hq-main/train/AC-SAM/work_dirs/myac_sam_b_cnn_muskstone768/vis \
---eval --restore-model /home/upc/Documents/SAM/sam-hq-main/sam-hq-main/train/AC-SAM/work_dirs/myac_sam_b_cnn_muskstone768/best_model.pth \
-#--visualize
